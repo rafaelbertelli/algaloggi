bff55d9f216e76e0f4a69fad53eec77c06dd6312
Rafael Borges <rafael.borges@luizalabs.com>
2021-05-20 18:27:55 -0300

    init project

---
+++ src/main/java/com/algaworks/algaloggi/AlgaloggiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaloggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaloggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaloggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algaloggi/AlgaloggiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaloggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaloggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
